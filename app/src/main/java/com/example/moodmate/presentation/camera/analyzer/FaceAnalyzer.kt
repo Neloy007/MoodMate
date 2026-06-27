d04c90d8c15002eed93d5d0a6344519d2662d25e
Neloy007 <neloymac24@gmail.com>
2026-06-28 03:33:56 +0600

    Integrate ML Kit face detection with CameraX analyzer

--- app/src/main/java/com/example/moodmate/presentation/camera/analyzer/FaceAnalyzer.kt
+++ app/src/main/java/com/example/moodmate/presentation/camera/analyzer/FaceAnalyzer.kt
@@ -1,14 +1,61 @@
 package com.example.moodmate.presentation.camera.analyzer
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 
 class FaceAnalyzer : ImageAnalysis.Analyzer {
-    override fun analyze(image: ImageProxy) {
 
-        Log.d("MoodMate", "Frame received")
+    private val options = FaceDetectorOptions.Builder()
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+        .enableTracking()
+        .build()
 
-        image.close()
+    private val detector = FaceDetection.getClient(options)
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+
+        val mediaImage = imageProxy.image
+
+        if (mediaImage != null) {
+
+            val image = InputImage.fromMediaImage(
+                mediaImage,
+                imageProxy.imageInfo.rotationDegrees
+            )
+
+            detector.process(image)
+                .addOnSuccessListener { faces ->
+
+                    Log.d(
+                        "MoodMate",
+                        "Faces detected: ${faces.size}"
+                    )
+
+                }
+                .addOnFailureListener {
+
+                    Log.e(
+                        "MoodMate",
+                        it.message ?: "Unknown error"
+                    )
+
+                }
+                .addOnCompleteListener {
+
+                    imageProxy.close()
+
+                }
+
+        } else {
+
+            imageProxy.close()
+
+        }
     }
 }
